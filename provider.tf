aef7ca59fade41396b8da05ce7081a3a55720d90
Cantooine <alican.uzun@bitaksi.com>
2023-12-16 20:19:40 +0300

    Add terraform and required provider version

--- provider.tf
+++ provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.5.0, < 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.30.0, < 5.32.0"
+    }
+  }
+}
+
 provider "aws" {
   region = "eu-west-1"
 }
